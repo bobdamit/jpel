0b8fe55ba95a69e7054f4ad72c0dac8791775887
Bob Damiano <bobd@rockscience.net>
2025-01-04 22:01:17 -0500

    boilerplate java project

---
+++ runner-java/bpjel-runner/src/main/java/net/rockscience/bpjel/runner/BpjelRunnerApplication.java
@@ -0,0 +1,13 @@
+package net.rockscience.bpjel.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpjelRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpjelRunnerApplication.class, args);
+	}
+
+}
---
+++ runner-java/bpjel-runner/src/test/java/net/rockscience/bpjel/runner/BpjelRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rockscience.bpjel.runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpjelRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
